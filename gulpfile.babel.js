4e70611cbed4b180c95a76c4818a2e84963dbcdb
Ladicle <Ladiclexxx@gmail.com>
2015-10-15 07:55:22 +0900

    introduce into the template library

---
+++ gulpfile.babel.js
@@ -0,0 +1,46 @@
+import gulp from 'gulp';
+import gulpLoadPlugins from 'gulp-load-plugins';
+import browserSync from 'browser-sync';
+
+const $ = gulpLoadPlugins();
+
+gulp.task('babel', () => {
+  gulp.src('./src/js/*.js')
+    .pipe($.plumber())
+    .pipe($.babel())
+    .pipe(gulp.dest('./dst'));
+});
+
+gulp.task('stylus', () => {
+  gulp.src('./src/css/*.stylus')
+    .pipe($.plumber())
+    .pipe($.stylus())
+    .pipe($.autoprefixer('last 2 version'))
+    .pipe(gulp.dest('./dst'))
+    .pipe(browserSync.reload({stream: true, once: true}))
+    ;
+});
+
+gulp.task('jade', () => {
+  gulp.src('./src/*.jade')
+    .pipe($.plumber())
+    .pipe($.jade())
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('serve', () => {
+  browserSync.init(null, {
+    server: {baseDir: './'}
+  });
+});
+
+gulp.task('reload', () => browserSync.reload());
+
+gulp.task('watch', () => {
+  gulp.watch('./src/js/*.js', ['babel', 'reload']);
+  gulp.watch('./src/css/*.stylus', ['stylus', 'reload']);
+  gulp.watch('./src/*.jade', ['jade', 'reload']);
+});
+
+gulp.task('build', ['babel', 'stylus', 'jade']);
+gulp.task('default', ['serve', 'build', 'watch']);
